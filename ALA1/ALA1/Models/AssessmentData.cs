9a703b91ff9901a12e2964fb36a3135d65f240fd
NagibatoSs <zhuravleva_02@mail.ru>
2024-01-15 22:31:40 +0400

    zagotovochka

---
+++ ALA1/ALA1/Models/AssessmentData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALA1.Models
+{
+    public class AssessmentData
+    {
+        public int СontrolsCount { get; set; }
+        public int RepeatingCount { get; set; }
+        public int WordsCount{ get; set; }
+        public int LongParagraphsCount { get; set; }
+        public double PatternMatch { get; set; } 
+        public double GroupsSize { get; set; } 
+
+        public AssessmentData()
+        {
+            СontrolsCount = 7;
+            RepeatingCount = 0;
+            WordsCount = 100;
+            LongParagraphsCount = 0;
+            PatternMatch = 1;
+            GroupsSize = 0.3;//30%
+        }
+    }
+}
