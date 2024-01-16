540dc9fe4bc902f0fb4f5d260b40f2524c1da015
NagibatoSs <zhuravleva_02@mail.ru>
2024-01-16 21:29:44 +0400

    new vs version project

---
+++ UIAssestment/UIAssestment/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UIAssestment.Models;
+
+namespace UIAssestment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHost;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHost)
+        {
+            _logger = logger;
+            _webHost = webHost;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(List<IFormFile> UploadedFiles)
+        {
+            string uploadFolder = Path.Combine(_webHost.WebRootPath, "Files");
+            if (!Directory.Exists(uploadFolder))
+            {
+                Directory.CreateDirectory(uploadFolder);
+            }
+
+            foreach (var uploadedFile in UploadedFiles)
+            {
+                string fileName = Path.GetFileName(uploadedFile.FileName);
+                string fileSavePath = Path.Combine(uploadFolder, fileName);
+                using (FileStream stream = new FileStream(fileSavePath, FileMode.Create))
+                {
+                    await uploadedFile.CopyToAsync(stream);
+                }
+                ViewBag.Message += fileName + " uploaded successfully ";
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UIAssestment/UIAssestment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UIAssestment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
